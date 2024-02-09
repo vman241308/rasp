cafabeab6a34ae29fa0d1d95f943273d3ca75ba8
BestHappy90619 <BestHappy90619@outlook.com>
2024-02-09 09:57:46 -0500

    Fix: tailwind

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,4 @@
-import { API_BASE_URL, DEBUG_MODE } from "@/utils/Constants";
-import { useEffect } from "react";
-
 const Home = () => {
-  useEffect(() => {
-    console.log("env>>>>", DEBUG_MODE, API_BASE_URL);
-  }, [])
 
   return (
     <>
