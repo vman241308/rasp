040205a6b2cde3d3679e8f3d9cf312451cc670f1
vman241308 <vman241308@gmail.com>
2024-02-09 13:30:22 -0500

    feat: Completed shift component

---
+++ src/components/RaspShift/index.tsx
@@ -0,0 +1,36 @@
+
+import React from 'react';
+
+export type ShiftComponentsProps = {
+    shift: {
+        day: string;
+        month: string;
+        name: string;
+        description: string;
+        duration: string;
+        status: number;
+    }
+}
+
+const RaspShift: React.FC<ShiftComponentsProps> = (props) => {
+    return (
+        <div className="flex flex-row p-4 border-solid border-b justify-between">
+            <div className='flex flex-col justify-between w-7'>
+                <p className="text-lg font-medium font-sans">{props.shift.day}</p>
+                <p className="text-sm font-normal font-sans">{props.shift.month}</p>
+            </div>
+
+            <div className='flex flex-col justify-between w-24'>
+                <p className="text-base font-normal font-sans">{props.shift.name}</p>
+                <p className="text-xs font-normal font-sans">{props.shift.description}</p>
+            </div>
+
+            <div className='flex flex-col justify-between w-24'>
+                <p className="text-base font-normal font-sans text-custom-blue bg-[#F3F8FC] rounded">{props.shift.duration}</p>
+                <p className="text-xs font-normal font-sans text-custom-gray">{props.shift.status === 0 ? "Confirmed" : "Published"}</p>
+            </div>
+        </div>
+    );
+};
+
+export default RaspShift;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
+    colors: {
+      "custom-gray": "#2A2A2A",
+      "custom-blue": "#4092F3"
+    }
   },
   plugins: [],
-};
-
+});
